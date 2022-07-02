e0345203d2aef529817c9ee47fde04dc1021f408
shvmdev <shvmdev9@gmail.com>
2022-07-02 14:16:27 +0530

    added till colr change

--- index.js
+++ index.js
@@ -1,18 +1,11 @@
-const lightblue = document.querySelector(".lbbox");
-const lightgreen = document.querySelector(".lgbox");
-const lightpink = document.querySelector(".lpbox");
-const bbox = document.querySelector(".bbox");
-
 const body = document.querySelector("body");
-lightblue.addEventListener('click', () => {
-    body.style.backgroundColor = "lightblue"
-})
-lightgreen.addEventListener('click', () => {
-    body.style.backgroundColor = 'lightgreen';
-})
-lightpink.addEventListener('click', () => {
-    body.style.backgroundColor='lightpink'
-})
-bbox.addEventListener('click', () => {
-    body.style.backgroundColor='#666'
-})
+const box = document.querySelectorAll(".color-box")
+console.log(box.length)
+for (let i = 0; i < box.length; i++){
+    let cbox = box[i];
+    cbox.addEventListener('click', (e) => {
+        let color = e.currentTarget.classList[1];
+        body.style.backgroundColor = color;
+        
+    })
+}
